30c617b9e8432903c7cba91dff5bcd91cd65df4a
Daniel Jobst <daniel.jobst@oth-regensburg.de>
2017-11-10 12:30:57 +0100

    Concatenation of actor asking (non working yet)

--- src/main/scala/actor/CartActor.scala
+++ src/main/scala/actor/CartActor.scala
@@ -1,11 +1,25 @@
 package actor
 
-import akka.actor.Actor
-import message.{AddItem, AddedItemEvent}
+import akka.actor.{Actor, ActorRef}
+import message._
+import akka.pattern.ask
+import akka.util.Timeout
+
+import scala.concurrent.duration._
+import scala.util.{Failure, Success}
+
 
 class CartActor(var id:String) extends Actor {
 
   var content = Map[String, Int]()
+
+  var paymentActor: ActorRef = _ // TODO!!!!!
+  var deliveryActor: ActorRef = _ // TODO!!!!!
+  var communicationsActor: ActorRef = _ // TODO!!!!!!
+
+  // additional two implicitly available objects needed for Futures
+  implicit val executionContext = context.dispatcher
+  implicit val timeout: Timeout = 5 seconds
 
   println(s"CartActor($id) created")
 
@@ -15,5 +29,22 @@ class CartActor(var id:String) extends Actor {
       content = content + (item -> 1)
       println(s"cart of $id is no containing: $content")
       sender() ! AddedItemEvent(item)
+
+    case CheckOut =>
+
+      (paymentActor ? CollectPayment(100.0, "DE12345", "Thanks")).mapTo[PaymentCollectedEvent]
+      .flatMap {
+        paymentEvent => {
+          (deliveryActor ? Deliver(content, "Unistr. 31, Regensburg")).mapTo[DeliveredEvent]
+        }
+      }.flatMap {
+        deliveredEvent =>
+          (communicationsActor ? SendConfirmation(s"user@mail.com", "Thanks for buying. Tracking id = ???")).mapTo[SentConfirmationEvent]
+      }.onComplete{
+        case Success(sentEvent) =>
+          println(s"Done all! Went well!")
+          sender() ! "Check out successful"  // TODO!!!!!!!
+        case Failure(failureMessage) =>
+      }
   }
 }
--- src/main/scala/message/Message.scala
+++ src/main/scala/message/Message.scala
@@ -5,7 +5,16 @@ trait Message
 sealed case class CreateCart(forId: String) extends Message
 sealed case class AddItem(toCart: String, item: String) extends Message
 
+sealed case class CheckOut() extends Message
+sealed case class CollectPayment(price: Double, accountId: String, text: String) extends Message
+sealed case class Deliver(items: Map[String, Int], address: String)
+sealed case class SendConfirmation(to: String, msg: String)
+
 
 trait EventMessage extends Message
 
 sealed case class AddedItemEvent(item:String) extends EventMessage
+sealed case class CheckedOutEvent() extends EventMessage
+sealed case class PaymentCollectedEvent(transactionId: String) extends EventMessage
+sealed case class DeliveredEvent(trackingId: String) extends EventMessage
+sealed case class SentConfirmationEvent() extends EventMessage
